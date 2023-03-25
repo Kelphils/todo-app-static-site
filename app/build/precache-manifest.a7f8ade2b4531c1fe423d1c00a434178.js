4cdeaca4694bcd4ed2d2dac09554720f4a1c1ff3
Kelphils <obiohakelvin96@gmail.com>
2023-03-25 02:20:44 +0100

    dns routing to todo static app

---
+++ app/build/precache-manifest.a7f8ade2b4531c1fe423d1c00a434178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3af0f3ed1709e9e581f904bcb9779a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a2ed48f335827c22ae",
+    "url": "/static/css/main.bcf89ff8.chunk.css"
+  },
+  {
+    "revision": "1c3875727f173f8c00d4",
+    "url": "/static/js/2.31157228.chunk.js"
+  },
+  {
+    "revision": "89a2ed48f335827c22ae",
+    "url": "/static/js/main.81551d1e.chunk.js"
+  },
+  {
+    "revision": "58345bc645d61e2a0641",
+    "url": "/static/js/runtime-main.37e0aca8.js"
+  }
+]);

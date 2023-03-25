545f6f64bc44acc9ea65367e11a00411cf85d86a
Kelvin Obioha <obiohakelvin96@gmail.com>
2023-03-25 20:43:58 +0100

    github workflow to upload artifacts to s3

--- terraform/global/s3/outputs.tf
+++ terraform/global/s3/outputs.tf
@@ -1,5 +1,5 @@
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.terraform_state.id
   description = "The ARN of the S3 bucket"
 }
 
--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,8 +1,9 @@
 
 output "bucket_name" {
-  value       = aws_s3_bucket.static-website-hosting.arn
+  value       = aws_s3_bucket.static-website-hosting.id
   description = "The name of the bucket"
 }
+
 
 output "bucket_website_endpoint" {
   value       = aws_s3_bucket.static-website-hosting.bucket_domain_name
